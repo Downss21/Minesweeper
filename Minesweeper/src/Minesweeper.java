7987655e9f3ff3f9e09525bd0c0fc1452fa7a1bf
downss21 <Downss21@stxtigers.com>
2021-03-18 14:56:27 -0400

    stuff

---
+++ Minesweeper/src/Minesweeper.java
@@ -0,0 +1,10 @@
+
+public class Minesweeper {
+	private char[][] mineBoard;
+	private char[][] clearBoard;
+	
+	public Minesweeper(int width, int height, int mines)
+	{
+		this.mineBoard = new char[height][width];
+	}
+}
