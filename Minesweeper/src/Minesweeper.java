654f36598e92324751ffbdf96441ca42c799312b
Sam Downs <downss21@stxtigers.com>
2021-03-19 13:30:37 -0400

    mines around each square is properly calculated now

--- Minesweeper/src/Minesweeper.java
+++ Minesweeper/src/Minesweeper.java
@@ -3,7 +3,14 @@ import java.util.Arrays;
 public class Minesweeper {
 	private char[][] mineBoard;
 	private char[][] clearBoard;
+	//mines is not the actual number of mines, its how many the player thinks are gone
+	//basically just actual number of mines minus number of flags
 	private int mines;
+	
+	private static final char UNTOUCHED = '-';
+	private static final char TOUCHED = '\u0000';
+	private static final char MINE = '*';
+	private static final char FLAG = 'F';
 	
 	public Minesweeper(int width, int height, int mines)
 	{
@@ -15,7 +22,7 @@ public class Minesweeper {
 			for (int j = 0; j < width; j++)
 			{
 				this.mineBoard[j][i] = '\u0000';
-				this.clearBoard[j][i] = '\u0000';
+				this.clearBoard[j][i] = UNTOUCHED;
 			}
 		}
 		
@@ -29,7 +36,26 @@ public class Minesweeper {
 		{
 			for (int j = 0; j < width; j++)
 			{
-				
+				if (mineBoard[i][j] == MINE)
+				{
+					continue;
+				}
+				Integer mineCount = 0;
+				for (int k = -1; k <= 1; k++)
+				{
+					for (int l = -1; l <=1; l++)
+					{
+						try
+						{
+							if(mineBoard[i + k][j + l] == MINE)
+							{
+								mineCount++;
+							}
+						}
+						catch (ArrayIndexOutOfBoundsException a) {}
+					}
+				}
+				mineBoard[i][j] = mineCount.toString().charAt(0);
 			}
 		}
 	}
@@ -45,9 +71,43 @@ public class Minesweeper {
 		}
 	}
 	
+	public void printBoard() {
+		for (int i = 0; i < mineBoard.length; i++)
+		{
+			for (int j = 0; j < mineBoard[i].length; j++)
+			{
+				if (clearBoard[i][j] == TOUCHED)
+				{
+					System.out.print(mineBoard[i][j]);
+				}
+				else
+				{
+					System.out.print(clearBoard[i][j]);
+				}
+			}
+			System.out.print("\n");
+		}
+	}
+	
+	public void toggleFlag(int y, int x)
+	{
+		if(clearBoard[y][x] == UNTOUCHED)
+		{
+			clearBoard[y][x] = FLAG;
+			mines--;
+		} else
+		{
+			if (clearBoard[y][x] == FLAG)
+			{
+				clearBoard[y][x] = UNTOUCHED;
+				mines++;
+			}
+		}
+	}
+	
 	public static void main(String[] args)
 	{
 		Minesweeper mines = new Minesweeper(10, 10, 10);
-		mines.printMines();
+		mines.printBoard();
 	}
 }
